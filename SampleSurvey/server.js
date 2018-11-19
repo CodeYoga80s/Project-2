8f67113a91ecccff2e4fc126e2a85f7b502efc41
trwylde <trwylde@gmail.com>
2018-11-18 20:43:26 -0500

    Made edits per Travis CI's complaints.

--- SampleSurvey/server.js
+++ SampleSurvey/server.js
@@ -1,25 +1,15 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
- 
-// create application/x-www-form-urlencoded parser
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
- 
-// parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
- 
-// parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
-
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
- 
 app.listen(PORT, function() {
-  console.log("Use url localhost:" + PORT)
+  console.log("Use url localhost:" + PORT);
 });
