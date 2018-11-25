67ecb712da0c73647760dc5b3016144d4b16bd52
trwylde <trwylde@gmail.com>
2018-11-25 15:46:17 -0500

    First draft of front-end.

---
+++ models/seeds.sql
@@ -0,0 +1,17 @@
+USE breeds_db;
+INSERT INTO breeds (breed_name, size, outdoor_space, child_safe, sociable, multi_animal_safe)
+VALUES ("Airedale Terrier", "2", "2", "3", "3", true);
+VALUES ("Akita", "2", "3", "1", "3", false);
+VALUES ("American Staffordshire Terrier", "2", "3", "3", "2", false);
+VALUES ("Basset Hound", "1", "2", "3", "1", true);
+VALUES ("Beagle", "1", "1", "3", "2", true);
+VALUES ("Bichon Frise", "1", "1", "3", "3", true);
+VALUES ("Bloodhound", "3", "2", "3", "3", true);
+VALUES ("Border Collie", "2", "3", "3", "2", true);
+VALUES ("Boston Terrier", "1", "1", "3", "3", true);
+VALUES ("Boxer", "2", "3", "3", "1", false);
+VALUES ("Bulldog", "1", "1", "1", "3", true);
+VALUES ("Cairn Terrier", "1", "1", "3", "1", true);
+VALUES ("Chihuahua", "1", "1", "1", "1", false);
+VALUES ("Chow Chow", "2", "1", "3", "2", true);
+VALUES ("Cocker Spaniel", "1", "1", "3", "3", true);
